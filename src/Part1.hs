1f7706c14604d6b5576d3b2fab4b15065b3ee393
Sjur Millidahl <sjur.millidahl@finn.no>
2018-10-31 13:52:28 +0100

    add numbers

--- src/Part1.hs
+++ src/Part1.hs
@@ -1,7 +1,16 @@
-module Part1 (count, sum) where
+module Part1 (helloWorld, add, count, sum) where
 
 import          Prelude hiding (count, sum)
 import          Numeric.Natural
+
+-- given a name (String) as input should create a personalized greeting
+-- e.g. `helloworld "Jens"` -> "Hello, Jens!"
+helloWorld :: String -> String
+helloWorld name = "Hello, " ++ name ++ "!"
+
+-- given two Floats, adds these together, resulting in another Float
+add :: Float -> Float -> Float
+add a b = a + b
 
 -- count things in a list
 count :: [a] -> Natural
--- test/Part1Spec.hs
+++ test/Part1Spec.hs
@@ -8,29 +8,55 @@ import           Numeric.Natural
 import           Prelude hiding (sum)
 
 
-countList :: [Int] -> Bool
-countList xs = count xs == (fromIntegral . length $ xs)
-
-sumList :: [Int] -> Bool
-sumList xs = sum xs == foldl (+) 0 xs
-
 main = do
-  unitTree <- unit
-  defaultMain $ tests unitTree
+    unitTree <- unit
+    defaultMain $ tests unitTree
+
+-- Property tests
 
 tests :: TestTree -> TestTree
 tests tree = testGroup "Part1" $ tree : properties
 
 properties :: [TestTree]
 properties =
-  [ QC.testProperty "count (property tests)" countList
+  [ QC.testProperty "helloWorld (property tests)" greetString
+  , QC.testProperty "add (property tests)" addProp
+  , QC.testProperty "count (property tests)" countList
   , QC.testProperty "sum (property tests)" sumList
   ]
 
+countList :: [Int] -> Bool
+countList xs = count xs == (fromIntegral . length $ xs)
+
+addProp :: (Float, Float) -> Bool
+addProp (a,b) = add a b == a + b
+
+sumList :: [Int] -> Bool
+sumList xs = sum xs == foldl (+) 0 xs
+
+greetString :: String -> Bool
+greetString s = length (helloWorld s) > length s
+
+-- Unit Tests
+
 unit :: IO TestTree
 unit = H.testSpec "count (unit tests)" $ do
+    helloWorldSpec
+    addNrSpec
     countSpec
     sumSpec
+
+helloWorldSpec :: Spec
+helloWorldSpec = describe "helloWorld" $ do
+  it "greets Anders" $ do
+    helloWorld "Anders" `shouldBe` "Hello, Anders!"
+
+addNrSpec :: Spec
+addNrSpec = describe "add" $ do
+  it "adds 3 and -3 to 0" $ do
+    add 3 (-3) `shouldBe` 0
+  it "adds 17 and 13 to 30" $ do
+    add 17 13 `shouldBe` 30
 
 countSpec :: Spec
 countSpec = describe "count" $ do
