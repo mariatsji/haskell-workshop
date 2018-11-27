4e3aace6eede37e06e042a6fa834487f4b5cbf01
Sjur Millidahl <sjur.millidahl@finn.no>
2018-11-27 13:32:41 +0100

    make tests green again

--- test/PartsSpec.hs
+++ test/PartsSpec.hs
@@ -7,7 +7,6 @@ import           Numeric.Natural
 import           Part1
 import           Part2
 import           Part3
-import           Prelude                           hiding ( sum )
 
 main = do
   unitTree <- unit
@@ -21,31 +20,30 @@ properties :: [TestTree]
 properties =
   [ QC.testProperty "helloWorld (property tests)" greetString
   , QC.testProperty "add (property tests)" addProp
-  , QC.testProperty "count (property tests)" countList
-  , QC.testProperty "sum (property tests)" sumList
+  , QC.testProperty "myLength (property tests)" countList
+  , QC.testProperty "mySum (property tests)" sumList
   ]
 
 countList :: [Int] -> Bool
-countList xs = count xs == (fromIntegral . length $ xs)
+countList xs = myLength xs == (fromIntegral . length $ xs)
 
 addProp :: (Float, Float) -> Bool
 addProp (a, b) = add a b == a + b
 
 sumList :: [Int] -> Bool
-sumList xs = sum xs == foldl (+) 0 xs
+sumList xs = mySum xs == foldl (+) 0 xs
 
 greetString :: String -> Bool
 greetString s = length (helloWorld s) > length s
 
 -- Unit Tests
 unit :: IO TestTree
-unit = H.testSpec "count (unit tests)" $ do
+unit = H.testSpec "Part1 (unit tests)" $ do
   helloWorldSpec
   addNrSpec
   countSpec
   sumSpec
-  applySpec
-  applysSpec
+  myMapSpec
   greaterSpec
   bitSpec
   refactorSpec
@@ -75,38 +73,28 @@ isSevenSpec = describe "isSeven" $ do
 countSpec :: Spec
 countSpec = describe "count" $ do
   it "counts the length of [] to 0" $ do
-    count [] `shouldBe` 0
+    myLength [] `shouldBe` 0
   it "counts the length of [2,5,2,3,6,3,4] to 7" $ do
-    count [2, 5, 2, 3, 6, 3, 4] `shouldBe` 7
+    myLength [2, 5, 2, 3, 6, 3, 4] `shouldBe` 7
 
 sumSpec :: Spec
-sumSpec = describe "sum" $ do
+sumSpec = describe "mySum" $ do
   it "sums [] to 0" $ do
     sum [] `shouldBe` 0
   it "sums [-5,2,13] to 10" $ do
     sum [-5, 2, 13] `shouldBe` 10
 
-applySpec :: Spec
-applySpec = describe "apply" $ do
-  it "applies the identity function with no apparent result to the number 15"
-    $ do
-        apply id 15 `shouldBe` 15
-  it "applies the succ function to 17 to find 18" $ do
-    apply succ 17 `shouldBe` 18
-  it "applies the length function to the String \"catnip\" to find 6" $ do
-    apply length "catnip" `shouldBe` 6
-
-applysSpec :: Spec
-applysSpec = describe "applys" $ do
+myMapSpec :: Spec
+myMapSpec = describe "myMap" $ do
   it "applies some function to the empty list returning the empty list" $ do
-    applys length ([] :: [String]) `shouldBe` []
+    myMap length ([] :: [String]) `shouldBe` []
   it
       "applies the identity function with no apprent result to the numbers [5,1,-2]"
     $ do
-        applys id [5, 1, -2] `shouldBe` [5, 1, -2]
+        myMap id [5, 1, -2] `shouldBe` [5, 1, -2]
   it "applies the length function on [\"catnip\", \"catfood\"] to find [6,7]"
     $ do
-        applys length ["catnip", "catfood"] `shouldBe` ([6, 7] :: [Int])
+        myMap length ["catnip", "catfood"] `shouldBe` ([6, 7] :: [Int])
 
 bitSpec :: Spec
 bitSpec = describe "bit datatype" $ do
