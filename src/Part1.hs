db4f0d18ef359048db3097eb05c7f45b7d90c04b
Fredrik Fossum <fredrik.fossum@finn.no>
2018-11-29 19:04:28 +0100

    Format part 1

--- src/Part1.hs
+++ src/Part1.hs
@@ -23,36 +23,37 @@ isSeven :: Integer -> Bool
 isSeven i = i == 7
 
 add1ToEveryElement :: [Int] -> [Int]
-add1ToEveryElement []     = []
-add1ToEveryElement (a:as) = (a + 1) : add1ToEveryElement as
+add1ToEveryElement []       = []
+add1ToEveryElement (a : as) = (a + 1) : add1ToEveryElement as
 
 negateEveryElement :: [Int] -> [Int]
-negateEveryElement []     = []
-negateEveryElement (a:as) = (-a) : negateEveryElement as
+negateEveryElement []       = []
+negateEveryElement (a : as) = (-a) : negateEveryElement as
 
 doubleEveryElement :: [Int] -> [Int]
-doubleEveryElement []     = []
-doubleEveryElement (a:as) = (a * 2) : doubleEveryElement as
+doubleEveryElement []       = []
+doubleEveryElement (a : as) = (a * 2) : doubleEveryElement as
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap _ [] = []
-myMap f (a:as) =  f a : myMap f as
+myMap _ []       = []
+myMap f (a : as) = f a : myMap f as
 
 add1WithMap :: [Int] -> [Int]
-add1WithMap = myMap (+1)
+add1WithMap = myMap (+ 1)
 
 negateWithMap :: [Int] -> [Int]
 negateWithMap = myMap negate
 
 doubleEveryElementWithMap :: [Int] -> [Int]
-doubleEveryElementWithMap = myMap (*2)
+doubleEveryElementWithMap = myMap (* 2)
 
 -- double every other element in a list, starting with the first
 -- e.g. doubleEveryOtherElement [1,2,3,4,5] == [2,2,6,4,10]
 doubleEveryOtherElement :: [Int] -> [Int]
-doubleEveryOtherElement [] = []
-doubleEveryOtherElement [a] = [a*2]
-doubleEveryOtherElement (a1:a2:as) = (a1 * 2) : a2 : (doubleEveryOtherElement as)
+doubleEveryOtherElement []  = []
+doubleEveryOtherElement [a] = [a * 2]
+doubleEveryOtherElement (a1 : a2 : as) =
+  (a1 * 2) : a2 : (doubleEveryOtherElement as)
 
 -- return True if there are no false elements in the list
 -- e.g.
@@ -92,47 +93,43 @@ anyTrueUsingFoldr as = foldr (||) False as
 -- use a tail recursive helper function (we've named it "loop")
 mySum :: [Int] -> Int
 mySum as = loop 0 as
-  where
-    loop acc []     = acc
-    loop acc (x:xs) = loop (acc + x) xs
+ where
+  loop acc []       = acc
+  loop acc (x : xs) = loop (acc + x) xs
 
 -- count elements in a list
 -- e.g. myLength [4,5,6] == 3
 myLength :: [a] -> Int
 myLength as = loop 0 as
-  where
-    loop acc []     = acc
-    loop acc (x:xs) = loop (acc + 1) xs
+ where
+  loop acc []       = acc
+  loop acc (x : xs) = loop (acc + 1) xs
 
 -- write a generalized function for folding in a left associative way
 -- e.g. myFoldl (*) 1 [2,3,4] == ((1 * 2) * 3) * 4
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl _ b [] = b
-myFoldl f b (a:as) = myFoldl f (f b a) as
+myFoldl _ b []       = b
+myFoldl f b (a : as) = myFoldl f (f b a) as
 
 sumUsingFoldl :: [Int] -> Int
 sumUsingFoldl = foldl (+) 0
 
 lengthUsingFoldl :: [a] -> Int
-lengthUsingFoldl as = foldl increment 0 as
-  where
-    increment b _ = b + 1
+lengthUsingFoldl as = foldl (\b _ -> b + 1) 0 as
+-- bonus questions:
+-- * what is the difference between left and right fold?
+-- * when should you use one over the other?
+
 
 -- Removing certain elements from a list
 -- aka Filtering
 noNegatives :: [Int] -> [Int]
-noNegatives [] = []
-noNegatives (a:as) =
-  if a >= 0
-    then a : noNegatives as
-    else noNegatives as
+noNegatives []       = []
+noNegatives (a : as) = if a >= 0 then a : noNegatives as else noNegatives as
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter _ []     = []
-myFilter f (a:as) =
-  if f a
-  then a : myFilter f as
-  else myFilter f as
+myFilter _ []       = []
+myFilter f (a : as) = if f a then a : myFilter f as else myFilter f as
 
 noNegativesUsingFilter :: [Int] -> [Int]
 noNegativesUsingFilter = myFilter (>= 0)
@@ -140,8 +137,8 @@ noNegativesUsingFilter = myFilter (>= 0)
 -- Append one linked list after another
 -- e.g. (myAppend [1,2,3] [4,5,6]) == [1,2,3,4,5,6]
 myAppend :: [a] -> [a] -> [a]
-myAppend []     bs = bs
-myAppend (a:as) bs = a : myAppend as bs
+myAppend []       bs = bs
+myAppend (a : as) bs = a : myAppend as bs
 
 myAppendUsingRightFold :: [a] -> [a] -> [a]
 myAppendUsingRightFold as bs = foldr (:) bs as
@@ -169,9 +166,5 @@ flatMap f as = myConcat (myMap f as)
 -- and the second element is from the second list
 -- e.g. (allCombinations [1,2] ['a', 'b']) == [(1,'a'), (1,'b'), (2,'a'), (2,'b')]
 -- use nested >>= calls
-allCombinations :: [a] -> [b] -> [(a,b)]
-allCombinations as bs =
-  as >>= \a ->
-    bs >>= \b ->
-      [(a,b)]
-
+allCombinations :: [a] -> [b] -> [(a, b)]
+allCombinations as bs = as >>= \a -> bs >>= \b -> [(a, b)]
