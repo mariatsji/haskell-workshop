dd862f84b345ad823f161a2db009b58e55461a20
Fredrik Fossum <fredrik.fossum@finn.no>
2018-11-29 18:47:50 +0100

    Text and examples

--- src/Part1.hs
+++ src/Part1.hs
@@ -7,7 +7,7 @@ greater :: Natural -> Natural -> Natural
 greater x y = if x > y then x else y
 
 -- given a name (String) as input should create a personalized greeting
--- e.g. helloworld "Jens" == "Hello, Jens!"
+-- e.g. helloWorld "Jens" == "Hello, Jens!"
 helloWorld :: String -> String
 helloWorld name = "Hello, " ++ name ++ "!"
 
@@ -16,6 +16,9 @@ add :: Float -> Float -> Float
 add a b = a + b
 
 -- check if a number is `7`
+-- e.g.
+--   isSeven 7 == True
+--   isSeven 6 == False
 isSeven :: Integer -> Bool
 isSeven i = i == 7
 
@@ -135,7 +138,7 @@ noNegativesUsingFilter :: [Int] -> [Int]
 noNegativesUsingFilter = myFilter (>= 0)
 
 -- Append one linked list after another
--- e.g. [1,2,3] ++ [4,5,6] == [1,2,3,4,5,6]
+-- e.g. (myAppend [1,2,3] [4,5,6]) == [1,2,3,4,5,6]
 myAppend :: [a] -> [a] -> [a]
 myAppend []     bs = bs
 myAppend (a:as) bs = a : myAppend as bs
@@ -144,31 +147,31 @@ myAppendUsingRightFold :: [a] -> [a] -> [a]
 myAppendUsingRightFold as bs = foldr (:) bs as
 
 -- concatenate a list of lists into a single list
--- e.g. concat [[1,2],[3,4],[5,6]] == [1,2,3,4,5,6]
+-- e.g. (concat [[1,2],[3,4],[5,6]]) == [1,2,3,4,5,6]
+-- tip: use the append function (`myAppend` or `(++)`) introduced earlier
 myConcat :: [[a]] -> [a]
-myConcat = foldr myAppend []
--- also known as "join" in haskell
--- in other languages known as "flatten"
--- tip: use the append function made earlier
+myConcat = foldr (++) []
+-- this operation is also known as `join` in haskell
+-- also known as "flatten" in some languages
 
 -- implement flatMap
 -- map and then flatten
--- e.g. flatMap (\x -> [x,x]) [1,2,3] == [1,1,2,2,3,3]
+-- e.g. (flatMap (\x -> [x,x]) [1,2,3]) == [1,1,2,2,3,3]
 -- notice the similarity to the "map" function
-myFlatMap :: (a -> [b]) -> [a] -> [b]
-myFlatMap f as = myConcat (myMap f as)
+flatMap :: (a -> [b]) -> [a] -> [b]
+flatMap f as = myConcat (myMap f as)
 -- this function is known as "bind" in haskell and is written >>=, which is an infix function
 -- however, the order of the first two arguments are reversed
--- so the example above becomes: [1,2,3] >>= (\x -> [x,x]) == [1,1,2,2,3,3]
+-- so the example above becomes: ([1,2,3] >>= (\x -> [x,x])) == [1,1,2,2,3,3]
 -- bonus question: why are the arguments reversed?
 
 -- create all possible pairs where the first element is from the first list
 -- and the second element is from the second list
--- e.g. allCombinations [1,2] ['a', 'b'] == [(1,'a'), (1,'b'), (2,'a'), (2,'b')]
+-- e.g. (allCombinations [1,2] ['a', 'b']) == [(1,'a'), (1,'b'), (2,'a'), (2,'b')]
 -- use nested >>= calls
 allCombinations :: [a] -> [b] -> [(a,b)]
 allCombinations as bs =
-  as >>= (\a ->
-  bs >>= (\b ->
-    [(a,b)]
-  ))
+  as >>= \a ->
+    bs >>= \b ->
+      [(a,b)]
+
