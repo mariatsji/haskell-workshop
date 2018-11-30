feb29147dd6d6d1b263f7d171f8859454a693aed
Fredrik Fossum <fredrik.fossum@finn.no>
2018-11-30 17:32:11 +0100

    Filter before fold. And add bunch of comments

--- src/Part1.hs
+++ src/Part1.hs
@@ -22,30 +22,51 @@ add a b = a + b
 isSeven :: Integer -> Bool
 isSeven i = i == 7
 
+-- add 1 to every element in a list of ints
+-- e.g. (add1ToEveryElement [1,2,3]) == [2,3,4]
+-- use a recursive function
 add1ToEveryElement :: [Int] -> [Int]
 add1ToEveryElement []       = []
 add1ToEveryElement (a : as) = (a + 1) : add1ToEveryElement as
 
+-- negate every element in a list of ints
+-- e.g. (negateEveryElement [1,-2,3]) == [-1,2,-3]
+-- this should be very similar to the previous task
 negateEveryElement :: [Int] -> [Int]
 negateEveryElement []       = []
-negateEveryElement (a : as) = (-a) : negateEveryElement as
+negateEveryElement (a : as) = -a : negateEveryElement as
 
+-- double every element in a list of ints
+-- e.g. (doubleEveryElement [1,2,3]) == [2,4,6]
 doubleEveryElement :: [Int] -> [Int]
 doubleEveryElement []       = []
 doubleEveryElement (a : as) = (a * 2) : doubleEveryElement as
 
+-- ok, writing a new recursive function every time is annoying
+-- let's write a generalized function for applying a transformation
+-- to every element of a list
 myMap :: (a -> b) -> [a] -> [b]
 myMap _ []       = []
 myMap f (a : as) = f a : myMap f as
+-- this function already exists in haskell, and is named `map`
+-- (there is also a more general `fmap` which can be used for
+-- more than just lists. For lists it is the same as `map`.)
 
+-- let's use our map function to reimplement the previous three functions
+-- how concisely can you write it?
+-- hint: for infix functions like + and *, you can wrap the operator and
+-- one operand in parantheses to create a partially applied function.
+-- e.g.
+--   (+ 5) is the same as (\x -> x + 5)
+--   (5 +) is the same as (\x -> 5 + x)
 add1WithMap :: [Int] -> [Int]
-add1WithMap = myMap (+ 1)
+add1WithMap as = map (+ 1) as
 
 negateWithMap :: [Int] -> [Int]
-negateWithMap = myMap negate
+negateWithMap as = map negate as
 
 doubleEveryElementWithMap :: [Int] -> [Int]
-doubleEveryElementWithMap = myMap (* 2)
+doubleEveryElementWithMap as = map (* 2) as
 
 -- double every other element in a list, starting with the first
 -- e.g. doubleEveryOtherElement [1,2,3,4,5] == [2,2,6,4,10]
@@ -54,6 +75,23 @@ doubleEveryOtherElement []  = []
 doubleEveryOtherElement [a] = [a * 2]
 doubleEveryOtherElement (a1 : a2 : as) =
   (a1 * 2) : a2 : (doubleEveryOtherElement as)
+
+doubleEveryOtherElementV2 :: [Int] -> [Int]
+doubleEveryOtherElementV2 as = zipWith doubleIfEvenIndex as [0..]
+  where
+    doubleIfEvenIndex a idx = if even idx then a * 2 else a
+
+-- Remove all negative numbers from a list of ints
+noNegatives :: [Int] -> [Int]
+noNegatives []       = []
+noNegatives (a : as) = if a >= 0 then a : noNegatives as else noNegatives as
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter _ []       = []
+myFilter f (a : as) = if f a then a : myFilter f as else myFilter f as
+
+noNegativesUsingFilter :: [Int] -> [Int]
+noNegativesUsingFilter = myFilter (>= 0)
 
 -- return True if there are no false elements in the list
 -- e.g.
@@ -119,20 +157,6 @@ lengthUsingFoldl as = foldl (\b _ -> b + 1) 0 as
 -- bonus questions:
 -- * what is the difference between left and right fold?
 -- * when should you use one over the other?
-
-
--- Removing certain elements from a list
--- aka Filtering
-noNegatives :: [Int] -> [Int]
-noNegatives []       = []
-noNegatives (a : as) = if a >= 0 then a : noNegatives as else noNegatives as
-
-myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter _ []       = []
-myFilter f (a : as) = if f a then a : myFilter f as else myFilter f as
-
-noNegativesUsingFilter :: [Int] -> [Int]
-noNegativesUsingFilter = myFilter (>= 0)
 
 -- Append one linked list after another
 -- this function already exists as ++ in haskell
